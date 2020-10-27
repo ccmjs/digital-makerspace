699474b4365dcf7410f02004e205b783a2e86bc4
akless <andre.kless@web.de>
2020-10-27 22:13:38 +0100

    update

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 ( async () => {
 
-  let url = 'https://ccm2.inf.h-brs.de', apps = 'dms-apps', components = 'dms-components', default_icon = './img/logo.png';
+  let url = 'https://ccm2.inf.h-brs.de', apps = 'dms-apps', tools = 'dms-components', default_icon = './img/logo.png';
+
+  if ( !sessionStorage.getItem( apps ) || !sessionStorage.getItem( tools ) ) {
+    Promise.all( [
+      ccm.get( { name: apps, url: url } ),
+      ccm.get( { name: tools, url: url } )
+    ] ).then( items => {
+      sessionStorage.setItem( apps, JSON.stringify( items[ 0 ] ) );
+      sessionStorage.setItem( tools, JSON.stringify( items[ 1 ] ) );
+    } );
+  }
 
   const params = new URL( window.location.href ).searchParams;
   let search = params.get( 'search' );
   if ( typeof search === 'string' ) {
     try {
-      search = atob( search );
+      search = JSON.parse( atob( search ) );
     } catch( e ) {}
     let items = [];
     const only = params.get( 'only' );
     if ( !only || only === 'apps' )
-      items = items.concat( await getItems( apps, search ) );
+      items = items.concat( getItems( apps, search ) );
     if ( !only || only === 'tools' )
-      items = items.concat( await getItems( components, search ) );
+      items = items.concat( getItems( tools, search ) );
+    items.sort( ( a, b ) => a.title.localeCompare( b.title ) );
 
     const list_elem = document.getElementById( 'results' );
-    items.forEach( ( { icon = default_icon, format, title, subject = '', created_at, creator } ) => {
+    items.forEach( ( { icon, format, title, subject = '', created_at, creator } ) => {
+      if ( !icon || !icon.trim() ) icon = default_icon;
+      created_at = moment( created_at ).fromNow();
       const is_app = format === 'application/json';
       const entry_elem = document.createElement( 'li' );
       entry_elem.classList.add( 'media', 'border-top', is_app ? 'bg-find' : 'bg-create' );
@@ -25,17 +38,28 @@
         <div class="media-body">
           <h5 class="mt-0 mb-1">${title} <span class="badge badge-${is_app ? 'success' : 'primary'}">${is_app ? 'App' : 'Tool'}</span></h5>
           ${subject ? subject + '<br>' : ''}
-          <small>Created at ${created_at} by ${creator}</small>
+          <small>Created ${created_at} by ${creator}</small>
         </div>
       `;
       list_elem.appendChild( entry_elem );
     } );
 
-    async function getItems( store, query ) {
-      return fetch( url, {
-        method: 'POST',
-        body: `{"store":"${store}","get":${query}}`
-      } ).then( response => response.json() );
+    function getItems( store, query ) {
+      store = JSON.parse( sessionStorage.getItem( store ) );
+      const items = [];
+      store.forEach( item => {
+        if ( typeof query !== 'string' ) {
+          for ( const key in query )
+            if ( !item[ key ].includes( query[ key ] ) )
+              return;
+          items.push( item );
+        }
+        else
+          for ( const key in item )
+            if ( typeof item[ key ] === 'string' && item[ key ].includes( query ) )
+              return items.push( item );
+      } )
+      return items;
     }
   }
 
