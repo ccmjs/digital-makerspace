3d4c063e923ff487b1f1826fda0ea2a96333033f
akless <andre.kless@web.de>
2020-11-09 11:15:28 +0100

    update

--- script.js
+++ script.js
@@ -459,8 +459,38 @@
       qr_code_elem.innerHTML = qr_code.createImgTag();
       document.querySelector( '#qr_code' ).appendChild( qr_code_elem.firstChild );
 
+      // fill the input fields of the form for edit app information with initial values
+      document.querySelector( '#title' ).value = app.title;
+      document.querySelector( '#icon' ).value = app.icon || '';
+      document.querySelector( '#subject' ).value = app.subject || '';
+      document.querySelector( '#description' ).value = app.description || '';
+      const tags = $( document.querySelector( '#tags' ) ).selectize( {
+        create: true,
+        plugins: [ 'remove_button' ],
+        valueField: 'value',
+        labelField: 'value',
+        searchField: 'value',
+        options: JSON.parse( sessionStorage.getItem( 'app-tag' ) ).map( tag => { return { value: tag }; } ),
+        items: app.tags
+      } )[ 0 ].selectize;
+      app.language.forEach( lang => document.querySelector( '#' + lang ).checked = true );
+
+      // set submit event of the form for edit app information
+      document.querySelector( '#edit-meta-form' ).addEventListener( 'submit', async event => {
+        event.preventDefault();
+        if ( !user ) return;
+        const app_meta = { key: key, language: [] };
+        $( '#edit-meta-form' ).serializeArray().forEach( ( { name, value } ) => value && ( name === 'language' ? app_meta[ name ].push( value ) : app_meta[ name ] = value ) );
+        app_meta.tags = tags.items;
+        const store = await ccm.store( { name: apps, url: url, token: user.token, realm: user.realm } );
+        const response = await store.set( app_meta );
+        sessionStorage.removeItem( apps );
+        response === key && location.reload();
+      } );
+
       // set click event for the "I agree" button
       document.querySelector( '#agree-btn' ).addEventListener( 'click', async () => {
+        if ( !user ) return;
         const store = await ccm.store( { name: apps, url: url, token: user.token, realm: user.realm } );
         const response = await store.set( { key: key, published: true } );
         sessionStorage.removeItem( apps );
